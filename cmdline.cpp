04881f9e17c79221b50582f589a9079fcfaf42bb
Kelan Albertson <kelan.albertson@utah.edu>
2022-01-11 21:13:17 -0700

    use_arguments works but still needs to be tested better

---
+++ cmdline.cpp
@@ -0,0 +1,35 @@
+//
+// Created by Kelan Albertson on 1/11/22.
+//
+
+#include <string>
+#include <iostream>
+#include "cmdline.h"
+
+int use_arguments (int argc, char *argv[]) {
+    bool  alreadyTested = false;
+    if (argc > 1) {
+        for (int i = 1; i < argc; ++i) {
+            std::string arg = argv[i];
+            if (arg == "--help") {
+                std::cout << "Enter '--help' for help and '--test' to test" << std::endl;
+                exit(0);
+            }
+            else if (arg == "--test") {
+                if (alreadyTested) {
+                    std::cerr << "Error: '--test' argument can only be used once" << std::endl;
+                    exit(1);
+                }
+                else {
+                    std::cout << "Tests passed" << std::endl;
+                    alreadyTested = true;
+                }
+            }
+            else {
+                std::cerr << "Error: invalid argument, can only handle '--help' or '--test'" << std::endl;
+                exit(1);
+            }
+        }
+    }
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Kelan Albertson on 1/11/22.
+//
+
+#include "cmdline.h"
+
+int main(int argc, char *argv[]) {
+
+    use_arguments(argc, argv);
+    return 0;
+}
